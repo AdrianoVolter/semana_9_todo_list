14b9e4b4b8bbc80bf2122f2b1b7811d6ebbfaf58
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-29 21:07:44 -0300

    Criando novo repo.

---
+++ src/Router.jsx
@@ -0,0 +1,32 @@
+/**
+ * Cadastre as rotas da sua aplicação!
+ * Lembrando: a estrutura do objeto que configura a rota é a seguinte:
+ *
+ * {
+ *   path: "/caminho-da-rota",
+ *   element: <ComponenteDaRota />,
+ * }
+ */
+
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "./App";
+import { Home } from "./pages/Home";
+import { QuemSou } from "./pages/QuemSou";
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      //  as rotas serão incluídas aqui!
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/quem-sou",
+        element: <QuemSou />,
+      },
+    ],
+  },
+]);
