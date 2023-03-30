56c92c49e37bab706e0cfa239b1616a1a97d1a93
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-29 21:24:31 -0300

    Exercício 2 semana 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from "./components/Header";
+import {Home} from "./pages/Home";
+import {QuemSou} from "./pages/QuemSou";
 
 export const App = () => {
   return (
     <div>
-      <Header />
+      <Header
+        title="React ToDo"
+      />
       <main className="container mt-4">
         {/* algo precisa ser modificado aqui... */}
+        <Home 
+
+        />
+        <QuemSou 
+
+        />
       </main>
     </div>
   );
