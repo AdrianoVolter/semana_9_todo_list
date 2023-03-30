d285ecd697fcdfd35318f821bf2d5ceddd3e65e4
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-29 21:56:15 -0300

    Outlet ex-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
-import {Home} from "./pages/Home";
-import {QuemSou} from "./pages/QuemSou";
 
 export const App = () => {
   return (
     <div>
-      <Header
-        title="React ToDo"
-      />
+      <Header/>
       <main className="container mt-4">
-        {/* algo precisa ser modificado aqui... */}
-        <Home 
-
-        />
-        <QuemSou 
-
-        />
+        <Outlet/>
       </main>
     </div>
   );
