f45530d39bdc092a688a23b25f107e81509caa1c
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-30 17:12:15 -0300

    Custon hook

---
+++ src/contexts/useToDos.jsx
@@ -0,0 +1,17 @@
+//crie e exporte uma função chamada useToDos. Esse será o nosso custom hook que dará acesso ao todoContext em todos os componentes que necessitarem dele.
+import React, { useContext } from 'react';
+
+
+function useToDos( ) {
+
+    const context = useContext(toDoContext);
+
+    return context;
+};
+
+
+
+
+export default useToDos;
+  
+  
