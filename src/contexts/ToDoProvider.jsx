79dbfc16511aaf8f5241e74056c8a8aed0b73660
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-31 14:10:18 -0300

    Ex 5 - Context  error: 1:1: expected declaration specifiers or '...' before numeric constant

--- src/contexts/ToDoProvider.jsx
+++ src/contexts/ToDoProvider.jsx
@@ -1,13 +1,56 @@
 //crie e exporte um contexto todoContext(use o createContext do React).
 import React, { createContext } from 'react';
+import { useState } from 'react';
 import { appRouter } from '../Router';
+//import { ToDoProvider } from './ToDoProvider';
+//import useToDos from './useToDos';
+//import { appRouter } from '../Router';
 
 export const toDosContext = createContext( );
 
-
 export function ToDoProvider({ children }) {
+
+    const [toDos, setToDos] = useState([
+        { id: 1, text: "Estudar React", done: false },
+        { id: 2, text: "Praticar com um projeto", done: true },
+        { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
+      ]);
+    
+      const addToDo = (text) => {
+        const newToDo = {
+          id: Date.now(),
+          text,
+          done: false,
+        };
+        setToDos([...toDos, newToDo]);
+      };
+    
+      const markTodo = (id) => {
+        const newToDos = toDos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, done: !todo.done };
+          }
+          return todo;
+        });
+        setToDos(newToDos);
+      };
+      
+      const completedToDos = toDos.filter((todo) => todo.done);
+      const pendingToDos = toDos.filter((todo) => !todo.done);
+    
+      
     return (
-        <toDosContext.Provider value={ appRouter } >
+
+        <toDosContext.Provider value={ {
+            appRouter,
+            markTodo,
+            addToDo,
+            completedToDos,
+            pendingToDos,
+            toDos,
+            setToDos,
+
+             }} >
            
             {children}
         </toDosContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
+//import { useState } from "react";
 import { TodoList } from "../components/TodoList";
 import { Formulario } from "../components/Formulario";
-import useToDos from "../contexts/useToDos";
+//import useToDos from "../contexts/useToDos";
+import { ToDoProvider } from "../contexts/ToDoProvider";
+import { toDosContext } from "../contexts/ToDoProvider";
 
 
 export const Home = () => {
-  const [toDos, setToDos] = useState([
-    { id: 1, text: "Estudar React", done: false },
-    { id: 2, text: "Praticar com um projeto", done: true },
-    { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
-  ]);
-
-  const addToDo = (text) => {
-    const newToDo = {
-      id: Date.now(),
-      text,
-      done: false,
-    };
-    setToDos([...toDos, newToDo]);
-  };
-
-  const markTodo = (id) => {
-    const newToDos = toDos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, done: !todo.done };
-      }
-      return todo;
-    });
-    setToDos(newToDos);
-  };
-
-  const completedToDos = toDos.filter((todo) => todo.done);
-  const pendingToDos = toDos.filter((todo) => !todo.done);
-
+  
+  
   return (
+    
     <ToDoProvider>
       <div className="row">
         <Formulario onSubmit={addToDo} />
