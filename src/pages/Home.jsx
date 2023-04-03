e976958a0706884b07142212d3ec0733e15304ba
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-03 09:24:24 -0300

    Exercicio 6 s-09  3 .0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
-// import { TodoList } from "../components/TodoList";
-// import { Formulario } from "../components/Formulario";
+import { useState } from "react";
+import { TodoList } from "../components/TodoList";
+import { Formulario } from "../components/Formulario";
 import { ToDoProvider } from "../contexts/ToDoProvider";
 
 export const Home = () => {
